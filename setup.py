87f14341f9ba1b597b44ebaafa2ed4ca6596323f
Arkaniva Sarkar <73361480+arkanivasarkar@users.noreply.github.com>
2024-11-03 19:32:37 +0100

    formatted as a pip installable python project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "math_quiz",                 
+    version = "0.0.1",                         
+    packages = find_packages(),                
+    author = "Arkaniva Sarkar",
+    author_email = "arkaniva.sarkar@fau.de",
+    long_description = open("README.md").read(), 
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/arkanivasarkar/dsss_homework_2",
+    license  =  "Apache 2.0",
+)
